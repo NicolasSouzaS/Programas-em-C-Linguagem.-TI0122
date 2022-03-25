d0615d1c377174965a724f0f9bac2756fd529d5e
NicolasSouzaS <nicolas.souzapb@gmail.com>
2022-03-25 09:19:27 -0300

    Arquivo README.MD

--- Exercícios/Desviofluxovol.c
+++ Exercícios/Desviofluxovol.c
@@ -11,6 +11,17 @@ void main()
     scanf("%f", &peso);
     printf("Informe a idade:\n");
     scanf("%d",&idade);
-    printf("Esta bem alimentado ? <1-SIM/0-NAO>");
+    printf("Esta bem alimentado ? <1-SIM/0-NAO>\n");
     scanf("%d", &bemalimentado);
+    printf("Está resfriado ? <1-SIM/ 0-NÃO>\n");
+    scanf("%d", &resfriado);
+
+        if (peso >= 50 && (idade >= 16 && idade <= 69)
+        && bemalimentado && !resfriado)
+        printf("O voluntario %s está apto!\n", nome);
+
+        else
+            printf("O voluntario %s NÃO está apto\n", nome);
+        
+        
 }
