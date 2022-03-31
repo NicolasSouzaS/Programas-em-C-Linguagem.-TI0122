3b90fe95849124deeeadfdabc5710f0492540c60
NicolasSouzaS <nicolas.souzapb@gmail.com>
2022-03-31 12:03:09 -0300

    Array

---
+++ 11-310322/array2.c
@@ -0,0 +1,16 @@
+/*
+Programa que exibe a soma
+utilizando um array.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculosarray.h"
+
+void main()
+{
+    int v[10] = {1,23,52,4,40,14,2,5,8,10};
+    soma(v,10);
+    maiorValor(v,10);
+    menorValor(v,10);
+}
